b5badf0d88eabe46c87c670de864b7dfd77bc9a2
Camilo <camilo.pulgarin11@gmail.com>
2020-04-28 23:47:14 -0500

    Hot fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for d in range(10):
-    for d1 in range( + 1, 10):
+    for d1 in range(d + 1, 10):
         if d != d1:
             if d == 8 and d1 == 9:
                 print('{}{}'.format(d, d1))
