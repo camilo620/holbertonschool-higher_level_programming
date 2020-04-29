f2df3a1e5f64c253000e4bb9cc8c55d7d3193677
Camilo <camilo.pulgarin11@gmail.com>
2020-04-28 23:54:05 -0500

    Add point 8 answer

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def lower(i):
+def islower(c):
     for l in range(ord('a'), ord('z') + 1):
-        if ord(i) == l:
+        if ord(c) == l:
             return True
     else:
         return False
