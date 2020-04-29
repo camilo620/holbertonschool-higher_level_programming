3a369a886b588b728c216d59247069a09d17b399
Camilo <camilo.pulgarin11@gmail.com>
2020-04-28 23:44:46 -0500

    Add point 7 answer

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for d in range(10):
+    for d1 in range( + 1, 10):
+        if d != d1:
+            if d == 8 and d1 == 9:
+                print('{}{}'.format(d, d1))
+            else:
+                print('{}{}, '.format(d, d1), end='')
