03ac26578f2911a6cef8fc6254788ab6eac14494
Camilo <camilo.pulgarin11@gmail.com>
2020-09-29 23:19:57 -0500

    Added all answers

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(linkedlist=[]):
+    linkedlist.append("Holberton")
+    return ", ".join(linkedlist)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" documentation
+"""
+
+
+class LockedClass:
+    """the documentation for the program
+    """
+    __slots__ = ["first_name"]
