f3ee42e1fb521a17d78a08053eb0372e6929b9e9
Camilo <camilo.pulgarin11@gmail.com>
2020-11-16 22:32:07 -0500

    answer point 1

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Inserts
+-- A new row
+INSERT INTO first_table VALUES (89, "Holberton School");
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- THis is kinda
+-- Boring to comment
+SELECT COUNT(*) FROM first_table WHERE id = 89;
